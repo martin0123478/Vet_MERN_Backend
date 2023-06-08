49f21311892d308e4b7464832fcd561f80c82adb
martin0123478 <martin_agui97@outlook.es>
2023-06-08 16:38:51 -0600

    feat:hash password

--- models/Veteriniario.js
+++ models/Veteriniario.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 import generarId from "../helpers/generarId.js";
+
 const veterinarioSchema = mongoose.Schema({
   nombre: {
     type: String,
@@ -35,5 +37,12 @@ const veterinarioSchema = mongoose.Schema({
   },
 });
 
+veterinarioSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 const Veterinario = mongoose.model("Veterinario", veterinarioSchema);
 export default Veterinario;
