efbebfb55a66b4649dfe04215b0a36df12fd55f5
martin0123478 <martin_agui97@outlook.es>
2023-06-09 13:24:01 -0600

    feat:reset password

--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -5,12 +5,17 @@ import {
   perfil,
   confirmar,
   autenticar,
+  olvidePassword,
+  comprobarToken,
+  nuevoPassword,
 } from "../controllers/veterinarioController.js";
 const router = express.Router();
 
 router.post("/", registrar);
 router.get("/confirmar/:token", confirmar);
 router.post("/login", autenticar);
-
+router.post("/olvide-password", olvidePassword);
+router.get("/olvide-password/:token", comprobarToken);
+router.post("/olvide-password/:token", nuevoPassword);
 router.get("/perfil", chekauth, perfil);
 export default router;
